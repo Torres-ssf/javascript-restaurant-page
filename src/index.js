ae56ed758da2aa49605842bf16c844ed52d3798b
Torres-ssf <sergio.uft@gmail.com>
2019-10-10 18:05:02 -0300

    feature(section2) Add the second section for the page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import section1 from './sections/section1.js';
+import section2 from './sections/section2.js';
 
 const rootContainer = document.getElementById("main");
 
 // const allContainers = document.createElement("h1");
 // allContainers.innerText = 'Hello There motherfocker'
 
-rootContainer.append(section1());
+rootContainer.append(
+    section1(),
+    section2()
+    );
 
 console.log(rootContainer);
 

fab947671a96c3ece4428a3973b8fa25a0798101
Torres-ssf <sergio.uft@gmail.com>
2019-11-30 12:32:51 -0300

    chore(fix): fix stickler errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import fullpage from 'fullpage.js/dist/fullpage.extensions.min'; // eslint-disable-line
+import fullpage from 'fullpage.js/dist/fullpage.extensions.min';  
 import sections from './modules/sections';
 
 const rootContainer = document.getElementById('fullpage');
@@ -7,7 +7,7 @@ rootContainer.append(
   sections.getSection1(),
   sections.getSection2(),
   sections.getSection3(),
-  sections.getSection4()
+  sections.getSection4() // eslint-disable-line
 );
 
 /* eslint-disable */
